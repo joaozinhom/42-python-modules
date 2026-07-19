7e87a8c87316a5db67a239e71c6e8cd1cb73630a
joaozinhom <joaomcr@proton.me>
2026-07-19 13:56:57 -0300

    chore: organization

---
+++ module00/main.py
@@ -0,0 +1,39 @@
+# from module00.ex0.ft_hello_garden import ft_hello_garden
+# def main():
+#     ft_hello_garden 
+
+# from module00.ex1.ft_garden_name import ft_garden_name
+# def main():
+#     ft_garden_name()
+
+# from module00.ex2.ft_plot_area import ft_plot_area
+# def main():
+#     ft_plot_area()
+
+# from module00.ex3.ft_harvest_total import ft_harvest_total
+# def main():
+#     ft_harvest_total()
+
+# from module00.ex4.ft_plant_age import ft_plant_age
+# def main():
+#     ft_plant_age()
+
+# from module00.ex5.ft_water_reminder import ft_water_reminder
+# def main():
+#     ft_water_reminder()
+
+# from module00.ex6.ft_count_harvest_iterative import ft_count_harvest_iterative
+# def main():
+#     ft_count_harvest_iterative()
+
+# from module00.ex6.ft_count_harvest_recursive import ft_count_harvest_recursive
+# def main():
+#     ft_count_harvest_recursive()
+
+# from module00.ex7.ft_seed_inventory import ft_seed_inventory
+# def main():
+#     ft_seed_inventory("tomato", 15, "packets")
+#     ft_seed_inventory("carrot", 8, "grams")
+#     ft_seed_inventory("lettuce", 12, "area")
+if __name__ == "__main__":
+    main()
